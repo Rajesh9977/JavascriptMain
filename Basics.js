61c99cf3a1e07c18ee40ef19efa279ba59ee0156
rajesh <rajeshrao7700@gmail.com>
2025-01-22 13:30:32 +0530

    I learnt this videos 6

--- Basics.js
+++ Basics.js
@@ -1,1 +1,72 @@
-console.log("hello from github")
+//Class 04 - let const var
+
+
+/*
+const accountId = 9977
+let accountEmail = "rajesh@gmail.com"
+var accountPass = 23232323
+
+accountEmail = "Arti@gmail"
+accountPass = 232323232
+
+console.log(accountEmail)
+console.log(accountPass)
+console.log(accountId)
+
+ */
+
+// Class 05 - Data types and ECMA script
+
+// alert("hello");
+
+// console.log(3+3);
+// console.log("Raj");
+
+// Always write code in single signle line
+
+// -> Code readability
+/*
+let name = "rajesh"  //string
+let age = 18 //Number range 2 to power 53
+let bol = true //boolean value
+null = standalone value
+undefined
+Symbol
+console.log(name);
+console.log(age);
+*/
+
+//Object
+
+
+//class 06 data type conversion
+/*
+let age = true;
+
+// let valueinNumber = Number(age);
+
+console.log(typeof age);
+console.log(age);
+console.log(typeof age)
+
+*/
+
+//07 Operation
+
+console.log(2+2);
+console.log(3-2);
+console.log(2*2);
+console.log(6/3);
+console.log(4%2);
+
+
+let str1 = "Hello";
+let str2 = "Raj"
+
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3")
