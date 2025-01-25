322b64a25d1fd1a9da7ec10b504aad1fa9da5a29
rajesh <rajeshrao7700@gmail.com>
2025-01-25 16:48:51 +0530

    control flow

--- Basics2.js
+++ Basics2.js
@@ -2,8 +2,8 @@
 
 //class 14 Array--------------------->
 
-const myArr = [0,1,2,3,4,5]
-
+// const myArr = [0,1,2,3,4,5]
+// 
 // myArr.push(2)
 // myArr.pop()
 // myArr.shift()  //from front value will go
@@ -85,7 +85,7 @@ const obj4 = {...obj2, ...obj3}
 console.log(obj4)   // most of time we will be using spread operator only.
 */
 
-
+/*
 const course = {
     coursename : "JS",
     Instructor : "Hitesh",
@@ -103,5 +103,110 @@ console.log(Instructor);
 console.log(Price);
 console.log(Lang);
 
+*/
+
+//Class 19 Function ---------------------------------------->
+
+/*
+function addtwoNumber(Num1, Num2){
+    console.log(Num1 + Num2);
+
+}
+
+addtwoNumber(3,4);
+addtwoNumber(5,8);
+addtwoNumber(10,87)
+
+function Name(Username){
+    if(!Username){
+        console.log("please enter username");
+        return
+
+    }
+    return `${Username} how are you`
+
+}
+
+console.log(Name("Rajesh"));
+console.log(Name())
+
+function calculate(...num1){
+    return num1
+}
+
+console.log(calculate(200,300,400));
+
+const user = {
+    username:"Rajesh",
+    phone: "9144997700",
+    Email: "Raj@gmail.com"
+}
+
+function contact(obj1){
+    console.log(`My contact name is ${obj1.username} and his phone number is ${obj1.phone}`);
+    
+}
+
+contact(user);
+
+you know you can call any obj in function like this.
+
+*/
 
 
+// Class 21 Global and local scope------------------------------>
+
+
+/*
+
+if (true){
+    let a = 10;
+    const b = 20;
+    var c = 30
+    
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+*/
+
+
+// Class 23 This and arrow function-------------------------------->
+
+// This----------------->
+
+/*const addtwo = (num1, num2) =>{
+    return num1+num2
+}
+
+console.log(addtwo(1,2))
+*/
+
+
+//Class 26 Control flow-------------------->
+
+if(true){
+    console.log("hello");
+    
+
+}
+
+const temp = 41;
+
+if(temp<50){
+    console.log("temp is less than 50");
+    
+}
+else{
+    console.log("temp is more than 50");
+    
+}
+
+const loggedinfromEmail = true;
+const loogedinFromGoogle = true;
+if(loogedinFromGoogle && loggedinfromEmail){
+    console.log("user logged in");
+    
+}
